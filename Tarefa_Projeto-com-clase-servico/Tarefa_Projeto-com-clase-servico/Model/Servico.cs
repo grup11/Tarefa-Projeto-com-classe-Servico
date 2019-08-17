22e8d6dd6afd1557ee4b610f71cfa2d4742e189a
dbonfleur <54067086+dbonfleur@users.noreply.github.com>
2019-08-17 17:37:28 -0300

    Add files via upload

---
+++ Tarefa_Projeto-com-clase-servico/Tarefa_Projeto-com-clase-servico/Model/Servico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarefa_Projeto_com_clase_servico.Model
+{
+    class Servico
+    {
+        public String Nome { get; set; }
+        public String Preco { get; set; }
+    }
+}
---
+++ Tarefa_Projeto-com-clase-servico/Tarefa_Projeto-com-clase-servico/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tarefa_Projeto_com_clase_servico.Model;
+
+namespace Tarefa_Projeto_com_clase_servico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Servico servico = new Servico();
+            Console.Write("Informe o nome do serviço: ");
+            servico.Nome = Console.ReadLine();
+            Console.Write($"Informe o preço do serviço de {servico.Nome}: ");
+            servico.Preco = Console.ReadLine();
+            Console.WriteLine($"Nome do serviço: {servico.Nome}\nSeu preço é: R${servico.Preco}");
+        }
+    }
+}
